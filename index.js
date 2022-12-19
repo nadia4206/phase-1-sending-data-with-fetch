f70971a21c3fac3de58dd9d9990e75fd618955ff
Nadia Virjee <nadvirjee@gmail.com>
2022-12-19 13:26:14 -0500

    commit tree a3608aa23f0155abf1c1dd7c26cf42a162c410c9

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+const userInfo = {
+    name: "",
+    email: ""
+}
+
+function submitData() {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userInfo)
+    })
+};
